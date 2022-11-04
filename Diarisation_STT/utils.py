5da8cb5c55285426f2c5900943ef0a65065bc8f4
himanshu03vsk <58275123+himanshu03vsk@users.noreply.github.com>
2022-11-04 20:50:29 +0530

    Added noise identification code

--- Diarisation_STT/utils.py
+++ Diarisation_STT/utils.py
@@ -163,9 +163,66 @@ def audio_splitter(ROOT, data_dir, audio_file_list, model, df):
 """Notes:
 Figure out way to do nisqa on >5sec samples
 """
+# Load the model package
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.data.distributed import DistributedSampler
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+import warnings
+
+from utils import create_folder, dump_config, process_idc
+import esc_config as config
+from sed_model import SEDWrapper, Ensemble_SEDWrapper
+from data_generator import ESC_Dataset
+from model.htsat import HTSAT_Swin_Transformer
+import prediction_denoise
 
 
-def audio_splitter(ROOT, data_dir, audio_file_list, model, df, output_dir, noise_model_path, noise_model_name):
+#This class must be initialized in the notebook to use it for noise classification
+class Audio_Classification:
+    def __init__(self, model_path, config):
+        super().__init__()
+
+        self.device = torch.device('cuda')
+        self.sed_model = HTSAT_Swin_Transformer(
+            spec_size=config.htsat_spec_size,
+            patch_size=config.htsat_patch_size,
+            in_chans=1,
+            num_classes=config.classes_num,
+            window_size=config.htsat_window_size,
+            config = config,
+            depths = config.htsat_depth,
+            embed_dim = config.htsat_dim,
+            patch_stride=config.htsat_stride,
+            num_heads=config.htsat_num_head
+        )
+        ckpt = torch.load(model_path, map_location="cpu")
+        temp_ckpt = {}
+        for key in ckpt["state_dict"]:
+            temp_ckpt[key[10:]] = ckpt['state_dict'][key]
+        self.sed_model.load_state_dict(temp_ckpt)
+        self.sed_model.to(self.device)
+        self.sed_model.eval()
+
+
+    def predict(self, audiofile):
+
+        if audiofile:
+            waveform, sr = librosa.load(audiofile, sr=32000)
+
+            with torch.no_grad():
+                x = torch.from_numpy(waveform).float().to(self.device)
+                output_dict = self.sed_model(x[None, :], None, True)
+                pred = output_dict['clipwise_output']
+                pred_post = pred[0].detach().cpu().numpy()
+                pred_label = np.argmax(pred_post)
+                pred_prob = np.max(pred_post)
+            return pred_label, pred_prob
+
+
+
+def audio_splitter(ROOT, data_dir, audio_file_list, timing_dir, splitted_audio_dir, model, df, output_dir,noise_classification_model, noise_model_path, noise_model_name, nisqa_kwargs, ):
     """
     This function will basically contain all the parellel part that was modified in MIRO Board, It will:
     1. Do the splitting
@@ -176,6 +233,16 @@ def audio_splitter(ROOT, data_dir, audio_file_list, model, df, output_dir, noise
     6. Bias Detection
     ROOT: your cwd
     data_dir: Directory of audio calls
+    audio_file_list: List of audio files
+    timing_dir: where to save the diarized timing txt files
+    splitter_audio_dir: Path to save the splitted audios
+    model: SST model object
+    df: df to operate on to product dataframe
+    output_dir: Not sure now
+    noise_classification_model: noise classification object
+    noise_model_path: path for directory where noise suppression model is stored
+    noise_model_name: name of the model file eg. unet_gg.h5
+    nisqa_kwargs: dictionary for nisqa model
     """
     from pydub import AudioSegment  # To read the WAV files
     import glob  # Kind of a regex to find any type of extension files in a folder
@@ -185,11 +252,11 @@ def audio_splitter(ROOT, data_dir, audio_file_list, model, df, output_dir, noise
         "deg": "",
         "pretrained_model": r"C:\Users\himan\Documents\Speech\nisqa\NISQA\weights\nisqa.tar",
         "output_dir": None,
-        "ms_channel": None,
-    }
+        "ms_channel": None,}
+         # provide this dictionary as argument
 
     timing_dir = os.path.join(
-        ROOT, "Timings"
+        ROOT, timing_dir
     )  # This specifies where to look for the timing file of the particular audio
     timings_list = glob.glob(
         f"{timing_dir}/*.txt"
@@ -208,15 +275,18 @@ def audio_splitter(ROOT, data_dir, audio_file_list, model, df, output_dir, noise
 
             audio_file = AudioSegment.from_file(audio)
             export_path = os.path.join(
-                ROOT, "splitted_audio"
-            )  # make splitted auido folder
+                ROOT, splitted_audio_dir
+            )  # make splitted audio folder
 
             for num, line in enumerate(f):
 
                 time = line.split(" ")
                 start = float(time[0])
                 end = float(time[1])
-                sliced_audio = audio_file[start * 1000 : end * 1000]
+                if start == 0.0:
+                    sliced_audio = audio_file[start * 1000 : end * 1000]
+                else:
+                    sliced_audio = audio_file[(start * 1000)+100 : end * 1000]
                 slice_audio_name = "{1}_{0}.wav".format(num, audio[:-4])
 
                 if sliced_audio.duration_seconds > 5.0:  # Make chunks
@@ -233,21 +303,32 @@ def audio_splitter(ROOT, data_dir, audio_file_list, model, df, output_dir, noise
 
                         print("exporting", chunk_name)
                         exp_file = chunk.export(
-                            os.path.join(output_dir, chunk_name), format="wav"
+                            os.path.join(splitted_audio_dir, chunk_name), format="wav"
                         )
+
+                        # exp_file = chunk.export(
+                            # os.path.join(output_dir, chunk_name), format="wav"
+                        # )
+
                         args["deg"] = exp_file
+
+                        #Do we have to use the nisqa and check if it has certain threshold and make it do noise classification
+
                         # TODO Use noise identification module here
-                        
+                        pred_label, pred_prob = noise_classification_model.predict(exp_file)   #make a mapping of labels to noise names
                         # TODO Use the audio file and supress the noise and either make a new directory or replace the file
-                        clean_audio = prediction(noise_model_path, noise_model_name, output_dir, clean_voice_folder, "audio_file_to_denoise",
-                                      'denoised_audio_file', sample_rate, 1.0, 8064, 8064,
+                        
+                        prediction_denoise.prediction(noise_model_path, noise_model_name, splitted_audio_dir, splitted_audio_dir, chunk_name,
+                                      chunk_name , 8000, 1.0, 8064, 8064,
                                       255, 63)
+                        clean_audio = splitted_audio_dir+ os.sep + "clean" + chunk_name
                         # ? For now I am taking only noise values but will make container for different parameters as well
                         # DO NISQA assessment on splitted audio file
                         model = nisqaModel(args)
                         nisqa_params = model.predict().noi_pred.values[0]
-                        transcript, confidence = transcribe_batch(exp_file, model=model)
+                        transcript, confidence = transcribe_batch(exp_file, model=model) #transcribe before denoise
 
+                        transcript, confidence = transcribe_batch(clean_audio, model=model) #transcribe after denoise
                         # TODO PESQ CODE HERE
 
                         # TODO BIAS CODE HERE
@@ -256,7 +337,7 @@ def audio_splitter(ROOT, data_dir, audio_file_list, model, df, output_dir, noise
 
                 else:
                     exp_file = sliced_audio.export(
-                        os.path.join(output_dir, slice_audio_name), format="wav"
+                        os.path.join(splitted_audio_dir, slice_audio_name), format="wav"
                     )
                     args["deg"] = exp_file
                     model = nisqaModel(args)
@@ -284,6 +365,8 @@ def audio_splitter(ROOT, data_dir, audio_file_list, model, df, output_dir, noise
                         "age": 0,
                         "gender": 0,
                         "accent": 0,
+                        "noise": pred_label,
+                        "n_confidenct": pred_prob
                     },
                     ignore_index=True,
                 )
